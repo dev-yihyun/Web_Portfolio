f2b35f414f050530250c5f6b1d67b0c0c11cb1a0
dev-yihyun <dev_yihyun@naver.com>
2020-09-14 02:19:20 +0900

    Add HTML File [index & css & javascript & image]

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+document.getElementById("modal_open_btn").onclick = function() {
+    document.getElementById("web_portfolio").style.display="block";
+}
+document.getElementById("modal_close_btn").onclick = function() {
+    document.getElementById("web_portfolio").style.display="none";
+}   
+document.getElementById("ing").onclick=function(){
+    alert('준비중인 프로젝트 입니다.');
+}
