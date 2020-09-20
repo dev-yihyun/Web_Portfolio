b1df656ef7f5bc251f4f3068c88de6d998a37b2d
dev-yihyun <dev_yihyun@naver.com>
2020-09-21 01:34:13 +0900

    Add Project[Hang_Man]

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,13 @@ document.getElementById("modal_open_btn").onclick = function() {
 document.getElementById("modal_close_btn").onclick = function() {
     document.getElementById("web_portfolio").style.display="none";
 }   
+document.getElementById("hangman_open_btn").onclick = function() {
+    document.getElementById("hangman").style.display="block";
+}
+document.getElementById("hangman_close_btn").onclick = function() {
+    document.getElementById("hangman").style.display="none";
+}   
+
 document.getElementById("ing").onclick=function(){
     alert('준비중인 프로젝트 입니다.');
 }
